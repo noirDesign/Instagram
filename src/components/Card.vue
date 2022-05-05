ea95de04e5a6331492c9e23cdda2f9a5b488a675
noirDesign <104095393+noirDesign@users.noreply.github.com>
2022-05-05 19:04:15 -0300

    vuexx

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+	<main
+		class="bg-primary rounded-20 overflow-hidden flex flex-row my-15"
+		@click="modal = true"
+	>
+		<img :src="item.thumbnailUrl" class="h-200 w-full object-cover" />
+		<div class="px-30 flex flex-col justify-center items-end w-200">
+			<h1 class="text-30">{{ item.title }}</h1>
+			<h2 class="text-15 text-terciary">${{ item.precio }}.00</h2>
+			<button class="px-5 py-2 rounded-20 bg-azul text-15 mt-15 w-fit">
+				comprar
+			</button>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	props: ['item'],
+	data() {
+		return {
+			modal: false,
+		};
+	},
+};
+</script>
